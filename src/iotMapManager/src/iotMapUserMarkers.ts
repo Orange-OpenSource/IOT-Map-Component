e8b219e5918169745b7c07f61f4a2f02f6982c17
sgateau <sandrine.gateau@orange.com>
2021-02-01 16:57:14 +0100

    IOTMAPCOMP-103 - Redim usermarker

--- src/iotMapManager/src/iotMapUserMarkers.ts
+++ src/iotMapManager/src/iotMapUserMarkers.ts
@@ -24,35 +24,31 @@ export class IotMapUserMarker {
   public getUserMarkerIcon(userMarker: IotUserMarker): L.DivIcon {
     const userSvg = IotMapCommonSvg.user;
 
-    // todo: shadow file
-    // const shadowFile = './assets/img/';
-    // const imgShadow = `<img class='shadowSelected' src='` + shadowFile + `'/>`;
+    // shadow file
+    const shadowFile = './assets/img/' + userSvg.shadow;
+    const imgShadow = `<img class='usermarkershadow' src='` + shadowFile + `'/>`;
 
-    let html = `<div class='markericon usermarkericon'>`;
+    let html = `<div class='usermarkericon'>`;
     if (userMarker.direction !== undefined) {
-      html = html
-        + `<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'>`
+      html += `<svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' viewBox='0 0 22 22'>`
         + userSvg.border
         + `</svg>`
-        + `<svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                              width="80"
-                              height="80"
-                              viewBox="-10 -10 42 42">`
+        + `<svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' viewBox='-3 -3 38 38'>`
         + userSvg.arrow + `transform='rotate(` + (userMarker.direction - 45) + ` 16 16)'/>`
         + `</svg>`;
     } else {
-      html = html
-        + `<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'>`
+      html += `<svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' viewBox='0 0 22 22'>`
         + userSvg.border + userSvg.inner
         + `</svg>`;
     }
-    const size = this.config.userMarker.size;
+    html += imgShadow;
+    //const size = this.config.userMarker.size;
 
     // creating icon
     return new L.DivIcon({
       className: 'my-custom-pin',
-      iconSize:     L.point(size.fullSvgWidth, size.fullSvgHeight), // size of the icon
-      iconAnchor:   L.point(size.fullSvgWidth / 2, size.fullSvgHeight / 2), // point of the icon which will correspond to marker's location
+      iconSize:     L.point(22, 22), // size of the icon
+      iconAnchor:   L.point(11, 11), // point of the icon which will correspond to marker's location
       html: html
     });
   }
