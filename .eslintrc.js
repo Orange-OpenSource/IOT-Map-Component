d4c3081bd040bc9997bd9d5c23a77d9ce36004b3
sgateau <sandrine.gateau@orange.com>
2021-02-08 11:56:27 +0100

    IOTMAPCOMP-25 - manual tab cluster KO

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
