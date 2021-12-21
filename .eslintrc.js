49925dfbe5e2aea90e4b8dca0da53bf0326703ca
Julien Déramond <julien.deramond@orange.com>
2021-12-21 19:57:48 +0100

    chore(deps): bump @storybook/* from 6.1.0 to 6.4.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,13 @@ module.exports = {
     commonjs: true,
     es2021: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'plugin:@typescript-eslint/recommended',
-    'standard'
-  ],
+  extends: ["plugin:vue/essential", "plugin:@typescript-eslint/recommended", "standard", "plugin:storybook/recommended"],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-    'eslint-plugin-tsdoc'
-  ],
+  plugins: ['vue', '@typescript-eslint', 'eslint-plugin-tsdoc'],
   rules: {
     'tsdoc/syntax': 'warn'
   }
-}
+};
