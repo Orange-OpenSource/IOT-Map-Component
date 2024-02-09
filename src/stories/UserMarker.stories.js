577d0d56b5d466de12dc3bf0907b2f3a82f8a8c7
Julien Déramond <julien.deramond@orange.com>
2024-02-09 09:53:06 +0100

    Finish UserMarker example

--- src/stories/UserMarker.stories.js
+++ src/stories/UserMarker.stories.js
@@ -1,13 +1,19 @@
 import {
   IotMapConfig,
   IotMapManager,
-  IotMapMarkerManager,
+  IotMapUserMarkerManager,
 } from "iotmapmanager/dist";
 import "./stories.css";
 
 const template = `<div id="iotMap" style="width: 1280px; height: 720px"></div>`;
 
-let markersList = [];
+let userMarker = 
+  {
+    location: [44.895, 4.87],
+    direction: 0,
+    accuracy: 150
+  }
+
 
 export default {
   title: "UserMarker",
@@ -19,14 +25,62 @@ export default {
 function update() {
   let config = new IotMapConfig();
   let mapManager = new IotMapManager(config);
-  let markerManager = new IotMapMarkerManager(mapManager, config);
+  let markerManager = new IotMapUserMarkerManager(mapManager, config);
   mapManager.init("iotMap");
-  markerManager.addMarkers(markersList);
+  markerManager.addUserMarker(userMarker);
   removeEventListener("DOMContentLoaded", update);
 }
 
 export const UserMarker = {
+  argTypes: {
+    accuracy: {
+      control: {
+        step: 20,
+        type: "number",
+        value: userMarker.accuracy,
+      },
+      name: "accuracy",
+    },
+    direction: {
+      control: {
+        max: 360,
+        min: 0,
+        step: 10,
+        type: "range",
+        value: userMarker.direction,
+      },
+      name: "direction",
+    },
+    locationLatitude: {
+      control: {
+        step: 0.001,
+        type: "number",
+        value: userMarker.location[1],
+      },
+      name: "location.latitude",
+    },
+    locationLongitude: {
+      control: {
+        step: 0.001,
+        type: "number",
+        value: userMarker.location[0],
+      },
+      name: "location.longitude",
+    },
+  },
   render: (args) => {
+    if (undefined !== args.accuracy) {
+      userMarker.accuracy = args.accuracy;
+    }
+    if (undefined !== args.direction) {
+      userMarker.direction = args.direction;
+    }
+    if (undefined !== args.locationLatitude) {
+      userMarker.location[1] = args.locationLatitude;
+    }
+    if (undefined !== args.locationLongitude) {
+      userMarker.location[0] = args.locationLongitude;
+    }
     addEventListener("DOMContentLoaded", update);
     return template;
   },
