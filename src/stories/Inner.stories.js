df3bbc6e733d7cd831ecc44f402f15be3c00beb4
Julien Déramond <julien.deramond@orange.com>
2024-02-09 08:48:10 +0100

    Finish Inner example

--- src/stories/Inner.stories.js
+++ src/stories/Inner.stories.js
@@ -1,85 +1,164 @@
-import { IotMapConfig, IotMapManager, IotMapMarkerManager } from 'iotmapmanager/dist';
-import './stories.css';
+import {
+  IotMapConfig,
+  IotMapManager,
+  IotMapMarkerManager,
+} from "iotmapmanager/dist";
+import "./stories.css";
 
 const template = `<div id="iotMap" style="width: 1280px; height: 720px"></div>`;
 
-let markersList = [{
-  id: 's1',
-  location: [
-    44.895,
-    4.87
-  ],
-  shape: {
-    type: 0, // select('Shape type',[ShapeType.circle, ShapeType.square],0,'item ' + id),
-    color: '#FFCC00',
-    anchored: false, // boolean('With anchor', false, 'item ' + id),
-    plain: false, // boolean('Plain shape', false, 'item ' + id),
-    accuracy: 150, // [number('accuracy', 150, {step: 20}, 'item 0'),],
-    direction: 0, // number('direction',0,{range: true,min: 0,max: 360,step: 10,}, 'item ' + id)
+let markersList = [
+  {
+    id: "s1",
+    location: [44.895, 4.87],
+    shape: {
+      type: 0,
+      color: "#FFCC00",
+      anchored: false,
+      plain: false,
+      accuracy: 150,
+      direction: 0,
+    },
+    inner: {
+      color: "green",
+      label: "J",
+      icon: "iotmap-icons-temperature",
+    },
   },
-  inner: {
-    color: 'green', // color('Inner color', 'green', 'item ' + id),
-    label: 'J', // text('Inner letter', 'J', 'item ' + id),
-    icon: 'iotmap-icons-temperature', //text('Inner icon', 'iotmap-icons-temperature', 'item ' + id),
-  },
-}];
+];
 
 export default {
-  title: 'Inner',
+  title: "Inner",
   parameters: {
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 };
 
 function update() {
   let config = new IotMapConfig();
   let mapManager = new IotMapManager(config);
-  let markerManager = new IotMapMarkerManager(mapManager, config)
-  mapManager.init('iotMap');
+  let markerManager = new IotMapMarkerManager(mapManager, config);
+  mapManager.init("iotMap");
   markerManager.addMarkers(markersList);
-  removeEventListener('DOMContentLoaded', update);
+  removeEventListener("DOMContentLoaded", update);
 }
 
 export const Inner = {
   argTypes: {
-    longitude: {
+    innerColor: {
       control: {
-        type: 'number',
-        step: 0.001,
-        value: markersList[0].location[0]
-      }
+        type: "color",
+        value: markersList[0].inner.color,
+      },
+      name: "inner.color",
     },
-    latitude: {
+    innerIcon: {
       control: {
-        type: 'number',
-        step: 0.001,
-        value: markersList[0].location[1]
-      }
+        type: "text",
+        value: markersList[0].inner.icon,
+      },
+      name: "inner.icon",
     },
-    shapeType: {
-      control: 'radio', options: ['Circle', 'Square'], value: markersList[0].shape.type === 0 ? 'Circle' : 'Square'
+    innerLabel: {
+      control: {
+        type: "text",
+        value: markersList[0].inner.label,
+      },
+      name: "inner.label",
+    },
+    locationLatitude: {
+      control: {
+        step: 0.001,
+        type: "number",
+        value: markersList[0].location[1],
+      },
+      name: "location.latitude",
+    },
+    locationLongitude: {
+      control: {
+        step: 0.001,
+        type: "number",
+        value: markersList[0].location[0],
+      },
+      name: "location.longitude",
+    },
+    shapeAccuracy: {
+      control: {
+        step: 20,
+        type: "number",
+        value: markersList[0].shape.accuracy,
+      },
+      name: "shape.accuracy",
+    },
+    shapeAnchored: {
+      control: "boolean",
+      name: "shape.anchored",
+      value: markersList[0].shape.anchored,
     },
     shapeColor: {
       control: {
-        type: 'color',
-        value: markersList[0].shape.color
-      }
+        type: "color",
+        value: markersList[0].shape.color,
+      },
+      name: "shape.color",
+    },
+    shapeDirection: {
+      control: {
+        max: 360,
+        min: 0,
+        step: 10,
+        type: "range",
+        value: markersList[0].shape.direction,
+      },
+      name: "shape.direction",
+    },
+    shapePlain: {
+      control: "boolean",
+      name: "shape.plain",
+      value: markersList[0].shape.plain,
+    },
+    shapeType: {
+      control: "radio",
+      name: "shape.type",
+      options: ["Circle", "Square"],
+      value: markersList[0].shape.type === 0 ? "Circle" : "Square",
     },
   },
   render: (args) => {
-    if (undefined !== args.longitude) {
-      markersList[0].location[0] = args.longitude;
+    if (undefined !== args.innerColor) {
+      markersList[0].inner.color = args.innerColor;
     }
-    if (undefined !== args.latitude) {
-      markersList[0].location[1] = args.latitude;
+    if (undefined !== args.innerIcon) {
+      markersList[0].inner.icon = args.innerIcon;
     }
-    if (undefined !== args.shapeType) {
-      markersList[0].shape.type = args.shapeType === 'Circle' ? 0 : 1;
+    if (undefined !== args.innerLabel) {
+      markersList[0].inner.label = args.innerLabel;
+    }
+    if (undefined !== args.locationLatitude) {
+      markersList[0].location[1] = args.locationLatitude;
+    }
+    if (undefined !== args.locationLongitude) {
+      markersList[0].location[0] = args.locationLongitude;
+    }
+    if (undefined !== args.shapeAccuracy) {
+      markersList[0].shape.accuracy = args.shapeAccuracy;
+    }
+    if (undefined !== args.shapeAnchored) {
+      markersList[0].shape.anchored = args.shapeAnchored;
     }
     if (undefined !== args.shapeColor) {
       markersList[0].shape.color = args.shapeColor;
     }
-    addEventListener('DOMContentLoaded', update);
-    return template
-  }
+    if (undefined !== args.shapeDirection) {
+      markersList[0].shape.direction = args.shapeDirection;
+    }
+    if (undefined !== args.shapePlain) {
+      markersList[0].shape.plain = args.shapePlain;
+    }
+    if (undefined !== args.shapeType) {
+      markersList[0].shape.type = args.shapeType === "Circle" ? 0 : 1;
+    }
+    addEventListener("DOMContentLoaded", update);
+    return template;
+  },
 };
