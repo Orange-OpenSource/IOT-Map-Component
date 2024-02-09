3b6ace352fedfc6dc3e344dd8eede762524c60f6
Julien Déramond <julien.deramond@orange.com>
2024-02-09 08:58:31 +0100

    Add all needed stories

---
+++ src/stories/GaugeWithInner.stories.js
@@ -0,0 +1,33 @@
+import {
+  IotMapConfig,
+  IotMapManager,
+  IotMapMarkerManager,
+} from "iotmapmanager/dist";
+import "./stories.css";
+
+const template = `<div id="iotMap" style="width: 1280px; height: 720px"></div>`;
+
+let markersList = [];
+
+export default {
+  title: "GaugeWithInner",
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+function update() {
+  let config = new IotMapConfig();
+  let mapManager = new IotMapManager(config);
+  let markerManager = new IotMapMarkerManager(mapManager, config);
+  mapManager.init("iotMap");
+  markerManager.addMarkers(markersList);
+  removeEventListener("DOMContentLoaded", update);
+}
+
+export const GaugeWithInner = {
+  render: (args) => {
+    addEventListener("DOMContentLoaded", update);
+    return template;
+  },
+};
