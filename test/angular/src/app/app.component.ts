a533ff6e2149acfd51e752561512aea3f1c6dd86
Louis-Maxime Piton <louismaxime.piton@orange.com>
2024-12-04 11:32:14 +0100

    chore(deps-dev): Bump Angular deps from 18.2.12 to 19.0.x

--- test/angular/src/app/app.component.ts
+++ test/angular/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { MapComponent } from './map/map.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [MapComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    imports: [MapComponent],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular';
