0238abfada237e5e10f06c2baa9f4218916e29da
Louis-Maxime Piton <louismaxime.piton@orange.com>
2024-12-12 12:18:53 +0100

    Change the test to use `waitForAsync` rather than `async`

--- test/angular/src/app/map/map.component.spec.ts
+++ test/angular/src/app/map/map.component.spec.ts
@@ -1,9 +1,9 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MapComponent } from './map.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
